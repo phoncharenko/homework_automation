eb3902799f87d564025cacc7482ba61c30df40d3
paul <p.goncharenko@smartweb.com.ua>
2018-11-10 14:59:54 +0200

    task_1 homework_5

--- homework_5/task_1.py
+++ homework_5/task_1.py
@@ -1,4 +1,5 @@
 import requests
+import ujson
 
 base_url = "http://pulse-rest-testing.herokuapp.com/"
 
@@ -7,7 +8,19 @@ payload = {
     'name': 'Paul'
 }
 
-create_user = requests.post(base_url+'roles/', data=payload)
+create_user = requests.post(base_url + 'roles/', data=payload)
 result = create_user.json()
-print(result)
+
 print(result['id'], result['name'])
+
+id = str(result['id'])
+check_user = requests.get(base_url + 'roles/' + id + '/')
+
+if check_user != "0":
+    print("User create is done. ID new user: ", id)
+
+check_user_list = ujson.loads(requests.get(base_url + 'roles/').text)
+print(check_user_list)
+
+for key in check_user_list:
+    if check_user_list["Paul"]: print("New user is in the list")
